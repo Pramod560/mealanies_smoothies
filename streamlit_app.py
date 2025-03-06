cd45c424b57a90c5139646bc4fca3459b30029f7
Pramod Raghuwanshi <47132601+Pramod560@users.noreply.github.com>
2025-03-06 08:28:01 +0530

    Update streamlit_app.py
    Changed the connection code because we moved from sis to snis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -9,7 +9,9 @@ st.write(
     """
 )
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection('snowflake')
+session  = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col('order_filled')==0).collect()
 
 if my_dataframe:
