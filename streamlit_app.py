10d9d53a570bafbb0ca6b89e8273dbaa0338c1cc
Pramod Raghuwanshi <47132601+Pramod560@users.noreply.github.com>
2025-03-11 06:53:42 +0530

    Update streamlit_app.py
    adding smoothiefruit api data

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,19 +10,14 @@ st.write(
     """
 )
 
-
-
 name_on_order= st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be: ", name_on_order)
-
-
 
 #session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients",
@@ -42,9 +37,10 @@ if ingredients_list:
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '"""+ name_on_order + "')"""
     #st.write(my_insert_stmt)
-    
-    
+        
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
